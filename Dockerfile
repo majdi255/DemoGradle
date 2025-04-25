1153881993a877400e0eefdb86efabe7d12215f6
AhmadMasry <good.dr.ahmad@gmail.com>
2025-04-25 23:36:38 +0300

    Update Springboot, Gradle and Java versions

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM openjdk:17
-ADD build/libs/demo-gradle*.jar demo-gradle.jar
-ENTRYPOINT [ "java","-jar","demo-gradle.jar" ]
--- src/main/java/com/example/demogradle/HelloController.java
+++ src/main/java/com/example/demogradle/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Spring Boot Gradle App! V3";
+		return "Spring Boot Gradle App!";
 	}
 
 }
